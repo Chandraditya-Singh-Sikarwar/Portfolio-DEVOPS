c647517ba63777ef88a05cb0d02dca9c20c87fd6
Chandraditya Singh Sikarwar <82415109+Chandraditya-Singh-Sikarwar@users.noreply.github.com>
2024-10-20 18:22:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// This script can be expanded to add dynamic features like form submission handling, animations, etc.
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you! Your message has been sent.');
+  });
+  
